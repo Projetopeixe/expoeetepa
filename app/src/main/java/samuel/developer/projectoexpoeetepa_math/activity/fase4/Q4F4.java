5cb49d64b77d2a9a45da59cbc4a743ca11e0db03
Samuel <samuelamorim865@gmail.com>
2019-11-22 17:26:39 -0300

    Changes

---
+++ app/src/main/java/samuel/developer/projectoexpoeetepa_math/activity/fase4/Q4F4.java
@@ -0,0 +1,15 @@
+package samuel.developer.projectoexpoeetepa_math.activity.fase4;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import samuel.developer.projectoexpoeetepa_math.R;
+
+public class Q4F4 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q4_f4);
+    }
+}
