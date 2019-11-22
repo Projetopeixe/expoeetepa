793a6123a55f5423a22322bd0e71fc990cd7cce4
Samuel <samuelamorim865@gmail.com>
2019-11-22 18:03:22 -0300

    Changes

---
+++ app/src/main/java/samuel/developer/projectoexpoeetepa_math/activity/fase7/Q7F7.java
@@ -0,0 +1,15 @@
+package samuel.developer.projectoexpoeetepa_math.activity.fase7;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import samuel.developer.projectoexpoeetepa_math.R;
+
+public class Q7F7 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_q7_f7);
+    }
+}
