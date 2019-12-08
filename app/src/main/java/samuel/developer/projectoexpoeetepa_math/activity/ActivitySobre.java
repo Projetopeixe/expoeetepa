d5386fb0429030f0458f412639ece572cf2b97ae
Samuel <samuelamorim865@gmail.com>
2019-12-08 15:56:17 -0300

    Changes

--- app/src/main/java/samuel/developer/projectoexpoeetepa_math/activity/ActivitySobre.java
+++ app/src/main/java/samuel/developer/projectoexpoeetepa_math/activity/ActivitySobre.java
@@ -13,6 +13,7 @@ public class ActivitySobre extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //setContentView(R.layout.activity_sobre);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         String descricao = "O app 'Easy Math' foi desenvolvido pela equipe de desenvolvedores abaixo da turma Técnico em Informática 2018, turno matutino, da Escola de Educação Profissional e Tecnológica do Estado do Pará (EETEPA) do municipio de Oriximiná- PA, com intuito de propor a interação entre a informática e o processo de ensino e aprendizagem da Matemática, visando a contribuição de meios tecnológicos nesse processo, de forma que seja visto resultados nítidos na aprendizagem dos usuários. O app conta com dez fases, contendo cinco perguntas em cada.";
         View sobre = new AboutPage(this)
@@ -31,7 +32,7 @@ public class ActivitySobre extends AppCompatActivity {
                 .addGroup("Luiz Eduardo Trindade da Silva")
                 .create();
         setContentView(sobre);
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
 
     }
 }
